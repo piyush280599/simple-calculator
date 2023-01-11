d0822fdd438ebcba5f0fd0c3d87c37051592aa8f
piyush280599 <118425491+piyush280599@users.noreply.github.com>
2023-01-11 23:01:39 +0530

    Substraction Multiplication and  Division Function added

--- src/main/java/org/example/service/Calculator.java
+++ src/main/java/org/example/service/Calculator.java
@@ -5,5 +5,17 @@ public class Calculator {
     public int add(int num1, int num2) {
         return num1 + num2;
     }
+    public int product(int num1, int num2 ){
+        return num1*num2;
+    }
+    public int substract(int num1, int num2 ){
+        return num1-num2;
+    }
+    public int div(int num1, int num2 ){
+        return num1/num2;
+    }
+    
+    
+
 
 }
