5958d436ce68ed61a699a25393dbfeaed69b42e5
Abhishek Saxena <5abhisheksaxena@gmail.com>
2023-01-11 19:39:43 +0530

    init commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.Scanner;
+
+// You can run the project either by clicking on run button
+// or
+// Using terminal, make sure that you open the terminal in the root folder.
+// ./gradlew -q --console plain run
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
